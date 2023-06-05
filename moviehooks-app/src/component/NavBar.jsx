b827ab43108a63f11d0252faabbb44e999430c45
Casanovaazyz <Casanova.azizo@gmail.com>
2023-06-05 14:16:54 +0100

    prj

---
+++ moviehooks-app/src/component/NavBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+
+function NavBar() {
+  return (
+    <div className='navbar'>
+    <Navbar bg="light" variant="light">
+    <Container>
+      <Navbar.Brand href="#App">Movie APP</Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Movies</Nav.Link>
+        <Nav.Link href="#features">Series</Nav.Link>
+        <Nav.Link href="#pricing">Anime</Nav.Link>
+      </Nav>
+    </Container>
+  </Navbar>
+      
+    </div>
+  )
+}
+
+export default NavBar
